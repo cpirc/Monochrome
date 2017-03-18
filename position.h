765a0db7969eb50ff6b3544fd6ff1a0942a88a50
Manik Charan <mkchan2951@gmail.com>
2017-03-19 01:22:08 +0530

    Indentation and function param fixes

--- position.h
+++ position.h
@@ -6,111 +6,102 @@
 
 /* Pieces on a chessboard. */
 enum Piece {
-        PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
+    PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
 };
 
 /* Colours on a chessboard: */
 enum Colour {
-        US, THEM
+    US, THEM
 };
 
 /* A chess position. */
 struct Position {
-        std::uint64_t pieces[6];  // Bitboards containing piece locations.
-        std::uint64_t colours[2]; // Bitboards containing colours of pieces.
-        Colour side;              // Side to move.
-        std::uint8_t castle;      // Castling rights.
-        char epsq;                // En passant square.
-        char fifty;               // Fifty-move rule counter.
-        std::uint64_t hash_key;   // Zobrist hash of the current position.
+    std::uint64_t pieces[6];  // Bitboards containing piece locations.
+    std::uint64_t colours[2]; // Bitboards containing colours of pieces.
+    Colour side;              // Side to move.
+    std::uint8_t castle;      // Castling rights.
+    char epsq;                // En passant square.
+    char fifty;               // Fifty-move rule counter.
+    std::uint64_t hash_key;   // Zobrist hash of the current position.
 };
 
 /* Check piece validity. */
 static inline bool piece_is_ok(const Piece p)
 {
-    if (p >= PAWN && p <= KING) {
-        return true;
-    }
-    return false;
+    return (p >= PAWN && p <= KING);
 }
 
 /* Check colour validity. */
 static inline bool colour_is_ok(const Colour c)
 {
-    if (c == US || c == THEM) {
-        return true;
-    }
-    return false;
+    return (c == US || c == THEM);
 }
 
 /* Check square validity. */
 static inline bool square_is_ok(const int sq)
 {
-    if (sq >= 0 && sq <= 63) {
-        return true;
-    }
-    return false;
+    return (sq >= 0 && sq <= 63);
 }
 
 /* Get a piece bitboard. */
-static inline std::uint64_t get_piece(const Board& b, const Piece p)
+static inline std::uint64_t get_piece(const Position& b, const Piece p)
 {
-        assert(piece_is_ok(p));
+    assert(piece_is_ok(p));
 
-        return b.pieces[static_cast<int>(p)];
+    return b.pieces[static_cast<int>(p)];
 }
 
 /* Get a colour bitboard. */
-static inline std::uint64_t get_colour(const Board& b, const Colour c)
+static inline std::uint64_t get_colour(const Position& b, const Colour c)
 {
-        assert(colour_is_ok(c));
+    assert(colour_is_ok(c));
 
-        return b.colours[static_cast<int>(c)];
+    return b.colours[static_cast<int>(c)];
 }
 
 /* Get a piece bitboard of a colour. */
-static inline std::uint64_t get_piece(const Board& b, const Piece p, const Colour c)
+static inline std::uint64_t get_piece(const Position& b, const Piece p, const Colour c)
 {
-        assert(piece_is_ok(p));
-        assert(colour_is_ok(c));
+    assert(piece_is_ok(p));
+    assert(colour_is_ok(c));
 
-        return get_piece(b, p) & get_colour(b, c);
+    return get_piece(b, p) & get_colour(b, c);
 }
 
 /* Set a bit in a piece bitboard. */
-static inline void set_bit(Board& b, const Piece p, const int sq)
+static inline void set_bit(Position& b, const Piece p, const int sq)
 {
-        assert(piece_is_ok(p));
-        assert(square_is_ok(sq));
+    assert(piece_is_ok(p));
+    assert(square_is_ok(sq));
 
-        b.pieces[static_cast<int>(p)] |= 1ULL << sq;
+    b.pieces[static_cast<int>(p)] |= 1ULL << sq;
 }
 
 /* Set a bit in a colour bitboard. */
-static inline void set_bit(Board& b, const Colour c, const int sq)
+static inline void set_bit(Position& b, const Colour c, const int sq)
 {
-        assert(colour_is_ok(c));
-        assert(square_is_ok(sq));
+    assert(colour_is_ok(c));
+    assert(square_is_ok(sq));
 
-        b.colours[static_cast<int>(c)] |= 1ULL << sq;
+    b.colours[static_cast<int>(c)] |= 1ULL << sq;
 }
 
 /* Clear a bit in a piece bitboard. */
-static inline void clear_bit(Board& b, const Piece p, const int sq)
+static inline void clear_bit(Position& b, const Piece p, const int sq)
 {
-        assert(piece_is_ok(p));
-        assert(square_is_ok(sq));
+    assert(piece_is_ok(p));
+    assert(square_is_ok(sq));
 
-        b.pieces[static_cast<int>(p)] &= ~(1ULL << sq);
+    b.pieces[static_cast<int>(p)] &= ~(1ULL << sq);
 }
 
 /* Clear a bit in a colour bitboard. */
-static inline void clear_bit(Board& b, const Colour c, const int sq)
+static inline void clear_bit(Position& b, const Colour c, const int sq)
 {
-        assert(colour_is_ok(c));
-        assert(square_is_ok(c));
+    assert(colour_is_ok(c));
+    assert(square_is_ok(c));
 
-        b.colours[static_cast<int>(c)] &= ~(1ULL << sq);
+    b.colours[static_cast<int>(c)] &= ~(1ULL << sq);
 }
 
 #endif
